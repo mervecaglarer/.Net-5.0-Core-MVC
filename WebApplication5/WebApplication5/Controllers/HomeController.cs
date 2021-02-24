f5c6a9717a79bc245d94e38ba361fddf4c5c77eb
mervecaglarer <mervecaglarer@hotmail.com>
2021-02-24 15:50:25 +0300

    Model example

---
+++ WebApplication5/WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var b = new List<Class>()
+            {
+                new Class() { BookID = 1, BookName = "Moby Dick", BookWriter = "Herman Menville"},
+                new Class() { BookID = 2, BookName = "Black Beauty", BookWriter = "Anna Sewell" },
+                new Class() { BookID = 3, BookName = "Le Petit Prince", BookWriter = "Antoine de Saint-Exupéry" }
+            };
+            return View(b);
+        }
+        
+    }
+}
---
+++ WebApplication5/WebApplication5/Models/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class Class
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+        public string BookWriter { get; set; }
+    }
+}
