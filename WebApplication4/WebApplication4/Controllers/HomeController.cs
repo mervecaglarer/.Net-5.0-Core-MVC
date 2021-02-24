1f562ef184ecd3f917fdec5fbbcdac520d75fa4c
mervecaglarer <mervecaglarer@hotmail.com>
2021-02-24 14:40:32 +0300

    Action types

---
+++ WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index1()
+        {
+            return View();
+        }
+        public IActionResult Index2() 
+        {
+            return View();
+        }
+        public int Index3()
+        {
+            return 23;
+        }
+    }
+}
